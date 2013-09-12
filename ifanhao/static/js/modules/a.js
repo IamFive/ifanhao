6b2238fa7c731999b9acc96cdca1145443e2870f
woo <iampurse@vip.qq.com>
2013-09-12 10:26:49 +0800

    add magnet

--- ifanhao/static/js/modules/a.js
+++ ifanhao/static/js/modules/a.js
@@ -5,10 +5,14 @@ A.search = function() {
 	var page = (element.length == 1) ? element.val() : '';
 	$("#f_search").attr("action", '/a/' + page);
 	return true;
-}
+};
 
 A.loadMagnet = function(code) {
 	$.get('/a/' + code + '/magnet', function(html){
-		$('#magnet-content').html(html)
+		$('#magnet-content').html(html);
 	});
-}
+};
+
+A.copy = function(text) {
+  window.prompt ("拷贝磁力链(请按下CTRL+C)", text);
+};
