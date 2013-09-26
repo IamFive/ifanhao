56648325fae28d4fa6ad53064b233feafad71bb9
woo <iampurse@vip.qq.com>
2013-09-26 21:42:51 +0800

    change prod folder

--- wsgi.py
+++ wsgi.py
@@ -6,9 +6,10 @@
 
 __test__ = False
 
+import os
 from ifanhao.common.app import startup_app
 from ifanhao.common.tools.env import ResourceLoader
 
-# os.environ.setdefault(ResourceLoader.ENV_VAR_NAME,
-#                     '/var/www/vhosts/icomic/resources/prod')
+os.environ.setdefault(ResourceLoader.ENV_VAR_NAME,
+                      '/home/www-data/ifanhao/resources/prod')
 application = startup_app()
